adf75b5a2adefe485ef0951cd2febc541f5d8286
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-01-19 19:35:13 -0500

    Added lines to demonstrate logic operations

---
+++ logic_if_while_loops.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 19:13:04 2018
+
+@author: Brian
+
+The purpose of this code is to practice with logical statements, if
+statements, and while loops.
+"""
+
+2 == 3
+2 == 2
+2 > 3
+2 >= 2
+2 != 3
+2 != 3 and 2 < 3
+2 != 3 or 2 > 3
+True
+False
+True and False
+True or False
+False and False
+
+# Checking if a string is in another string
+str1 = 'hello'
+'h' in str1
+'hell' in str1
+'q' in str1
+'q' not in str1
+
+# Checking if a value is in a list
+primes = [2, 3, 5, 7, 11, 13, 17]
+3 in primes
+4 in primes
+4 not in primes
+
