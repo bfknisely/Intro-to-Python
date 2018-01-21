d67d6286e312e3b90aba4103a19896c7d45792f2
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-01-21 12:02:06 -0500

    Added more comments about reading files

--- 05_read_file_write_file.py
+++ 05_read_file_write_file.py
@@ -5,13 +5,16 @@ Created on Fri Jan 19 19:38:08 2018
 @author: Brian
 """
 
-fname = 'acc2.dat'
-infile = open(fname, "r")
-longStr = infile.read()
-data = longStr.split('\n')
-infile.close()  # VERY IMPORTANT
+# opening a 1D list of numbers
+
+fname = 'acc2.dat'  # select name of file
+infile = open(fname, "r")  # open file with "read" access
+longStr = infile.read()  # read contents of file into one long string
+data = longStr.split('\n')  # separate long string into discrete values
+# split the string at the "newline" character
+infile.close()  # VERY IMPORTANT - close the file when you're done with it
 
 data
-len(data)
-min(data)
-max(data)
+len(data)  # look at length of data
+min(data)  # find minimum value
+max(data)  # find maximum value
