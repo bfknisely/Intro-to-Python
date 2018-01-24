45079038b2a8ac34efeb4837021c370f37756b79
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-01-24 10:04:54 -0500

    Renamed strings file to include info about input and data types as well

---
+++ 01_strings_input_type.py
@@ -0,0 +1,44 @@
+"""
+Created on Fri Jan 19 19:13:04 2018
+
+@author: Brian
+
+The purpose of this code is to practice with strings, input, and data types
+"""
+
+# Strings
+# Define strings with either single quotes or double quotes
+
+str1 = 'hello'
+str2 = "there"
+str1 + str2
+str1 + ' ' + str2
+
+str3 = str1 + ' ' + str2
+
+str1 + ‘ = ‘ a
+
+print(str1)
+
+len(str1)
+
+str1.split(‘e’)
+str3 = ‘How do you do?’
+str3.split(‘ ‘)
+
+str3.upper()
+
+Input/Eval Input
+
+c = input(‘Enter a number: ‘)
+
+type(c)
+
+d = eval(input(‘Enter a number: ‘))
+
+type(d)
+type('3')
+type(3)
+type(3.1)
+int(3.1)
+float(3)
