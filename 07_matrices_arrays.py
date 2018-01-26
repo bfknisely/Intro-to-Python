5321ae079ef00c39546b8714cbe0708d55cdded0
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-01-25 21:09:42 -0500

    Added further comments, dot product info, elementwise addition/subtraction

--- 07_matrices_arrays.py
+++ 07_matrices_arrays.py
@@ -31,6 +31,7 @@ a[:, 0]
 
 # get ALL values in last row
 a[-1, :]
+a[-1]  # same as line above; get all values in last row
 
 a.T  # take transpose of matrix with the "T" method
 
@@ -40,6 +41,12 @@ a.i  # methods/functions are case-sensitive; must use uppercase I for inverse
 b = np.mat('1, 3; 5, 7')  # create another matrix
 
 a*b  # matrix multiplication is with asterisk (star) operator
+
+a+b  # elementwise addition of matrices
+a+2  # elementwise addition of matrix with scalar
+
+a-b  # elementwise subtraction of matrices
+a-4  # subtract scalar from matrix
 
 np.multiply(a, 3)  # elementwise multiplication of matrix with scalar
 a*3  # elementwise multiplication of matrix with scalar also works with *
@@ -57,8 +64,9 @@ c = np.array([[1, 2], [3, 4]])  # create 2 x 2 array (note the square brackets)
 print(c)  # print out the array
 type(c)  # show that it is an ndarray
 
-d = np.array([[1, 3], [5, 7]])  # create another array
+d = np.array([[1., 3], [5, 7]])  # create another array
 
+# can easily form arrays using lists
 np.array(a)  # can convert a matrix into an array easily
 np.mat(c)  # and vise versa
 
@@ -74,7 +82,6 @@ np.identity(4)  # create identity matrix array of size 4 x 4
 np.ones(5)  # create 1D array of ones with 5 columns
 np.ones([1, 5])  # create array of ones with 1 row, 5 columns
 np.ones([3, 5])  # create 2D array of ones with 3 rows, 5 columns
-
 
 # Indexing arrays and assignment
 c[0, 0]  # get element at 0th row, 0th column of array c
@@ -94,6 +101,7 @@ ar[-1]  # index the last row, all columns
 
 # assigning individual values
 z[-1, -1] = 99  # assign last row, last column of z to be 99
+d[1, 1] = 'cow'  # note, arrays can only contain one data type (e.g. float)
 print(z)
 z[0] = 33  # assign all values in first row of z to be 33
 print(z)
@@ -119,15 +127,25 @@ print(z)
 a*b  # matrix multiplication is with asterisk (star) operator
 a@b  # array matrix multiplication is with @ operator
 
+# Dot products
+np.dot(a, b)
+np.dot(c, d)
+
 # Elementwise operations
 # All regular math operators (e.g. +, -, *, /, **) are elementwise on arrays
 # For matrices, you must use functions like multiply or power
 2*c  # elementwise multiply 2 by array
+2*a  # elementwise multiply 2 by matrix
 # order does not matter with scalar multiplication
 c*2  # elementwise multiply array by 2
 c+2  # elementwise add array plus two
+a+b  # elementwise addition/subtraction works for matrices too
 c/7  # elementwise divide array by 7
-c**2  # take each element array to power of two
+c**2  # take each element in array to power of two
 
-a.nonzero()  # get nonzero elements of matrix
-c.nonzero()  # get nonzero elements of array
+print(z)  # recall what z looks like
+q = z.nonzero()  # get nonzero elements of array
+# results are paired row-column coordinates
+z[q]  # indexes nonzero elements in array
+
+np.linalg.det(a)  # get determinant of square matrix
